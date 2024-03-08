ef63c8d47df871d9abc379cc026992031d33fc19
surarats <b.surarat@gmail.com>
2024-03-08 09:19:30 +0700

    feat: add cart page, responsive for mobile and desktop

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Layout from "./components/Layout.jsx";
 import Home from "./pages/Home.jsx";
-import Product from "./pages/Product.jsx";
 
 const router = createBrowserRouter([
   {
     element: <Layout />,
-    children: [{ path: "/", element: <Home /> },
-    { path: "/product", element: <Product /> }],
+    children: [{ path: "/", element: <Home /> }],
   },
 ]);
 
