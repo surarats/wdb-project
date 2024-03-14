ff50b59c1d4a21c48c5e9772758968de6e0959a0
Konpwomi <patiphan.arp@gmail.com>
2024-03-14 15:46:27 +0700

    Fetching data from API

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,26 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const permalink = "shirts-rayon-long-sleeve-blouse";
+
 function ProductDetail() {
+  const [products, setProducts] = useState([]);
+  console.log(products)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await axios.get(
+          `https://api.storefront.wdb.skooldio.dev/products/${permalink}`
+        );
+        setProducts(result.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <div>ProductDetail</div>
