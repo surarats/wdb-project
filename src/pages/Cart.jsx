bfcc528529f77c6c0865d0fff35dda1652b01985
surarats <b.surarat@gmail.com>
2024-03-16 22:45:25 +0700

    feat: initial product card component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MyCart from "../components/MyCart";
+import ProductCard from "../components/ProductCard";
 
 function Cart() {
   return (
     <div className="bg-[#fafafa] lg:min-h-screen">
       <div className="mx-4 lg:mx-auto lg:max-w-[1600px]">
         <MyCart />
+        <ProductCard />
       </div>
     </div>
   );
