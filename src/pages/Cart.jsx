46643b853fc12478e765979763229b192b873ea3
surarats <b.surarat@gmail.com>
2024-03-17 19:36:47 +0700

    feat: add product card global component & product card recommend

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import MyCart from "../components/MyCart";
-import ProductCard from "../components/ProductCard";
 
 function Cart() {
   return (
     <div className="bg-[#fafafa] lg:min-h-screen">
-      <div className="mx-4 lg:mx-auto lg:max-w-[1600px]">
+      <div className="mx-4 lg:mx-auto lg:max-w-[1600px] pb-10">
         <MyCart />
-        <ProductCard />
       </div>
     </div>
   );
